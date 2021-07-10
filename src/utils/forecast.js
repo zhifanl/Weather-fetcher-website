45d9fbe2f3438b82bd7a7043046270c527914e55
zhifanl <64334331+zhifanl@users.noreply.github.com>
2021-07-10 14:13:05 -0600

    .

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
             // console.log(response.body.error)
             callback("Unable to find location",undefined)
         } else
-            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees.")
+            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees. The humidity is "+body.current.humidity+"%.")
     })
 }
 
